2b968858f84d77c13f9727d06374d7d64d6694ae
Flavio F. Contreras-Torres <contreras.flavio@tec.mx>
2026-05-22 18:29:32 -0600

    chore: add project version metadata

---
+++ version.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+"""Package version metadata."""
+
+__version__ = "0.1.1"
+PROJECT_NAME = "MOSAIC"
+PROJECT_VERSION = __version__
+PROJECT_STATUS = "alpha"
+PROJECT_LICENSE = "LGPL-3.0-or-later"
+
+__all__ = ["__version__", "PROJECT_NAME", "PROJECT_VERSION", "PROJECT_STATUS","PROJECT_LICENSE",]
