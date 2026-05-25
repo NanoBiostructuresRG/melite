52afb1fd7554ee70ec0b0fb5ff9d3abe10dee308
Flavio F. Contreras-Torres <contreras.flavio@tec.mx>
2026-05-25 09:09:08 -0600

    docs: rewrite README outside-in, add CHANGELOG v0.1.7, bump version to 0.1.7

--- mosaic/version.py
+++ mosaic/version.py
@@ -6,7 +6,7 @@ It is read by ``hatchling`` at build time via ``[tool.hatch.version]``
 and imported by ``result_manager`` to stamp generated reports.
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 PROJECT_NAME = "MOSAIC"
 PROJECT_VERSION = __version__
 PROJECT_STATUS = "alpha"
